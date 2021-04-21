1af660911bf547f86072eb1cce5484fc4114540d
Chrisando Pramudhita <chrsep@protonmail.com>
2021-04-22 00:08:20 +0700

    add more stuffs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,48 @@
+const plugins = [
+  "gatsby-plugin-postcss",
+  "gatsby-plugin-gatsby-cloud",
+  "gatsby-plugin-image",
+  "gatsby-plugin-react-helmet",
+  "gatsby-plugin-sitemap",
+  {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      icon: "src/images/icon.png",
+    },
+  },
+  "gatsby-plugin-sharp",
+  "gatsby-transformer-sharp",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "images",
+      path: "./src/images/",
+    },
+    __key: "images",
+  },
+  {
+    resolve: `gatsby-plugin-typegen`,
+    options: {
+      outputPath: `src/__generated__/gatsby-types.d.ts`,
+      emitSchema: {
+        "src/__generated__/gatsby-schema.graphql": true,
+      },
+    },
+  },
+  `gatsby-plugin-tsconfig-paths`
+]
+
+if (process.env.NODE_ENV === "production") {
+  plugins.push("gatsby-plugin-preact")
+}
+
 module.exports = {
+  flags: {
+    FAST_DEV: true,
+  },
   siteMetadata: {
+    siteUrl: "https://wedding.chrsep.dev",
     title: "wedding",
   },
-  plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+  plugins
 };
