ec2caf7ab16234ad55318d64ecf2071687ae190b
Chrisando Pramudhita <chrsep@protonmail.com>
2021-04-23 00:04:30 +0700

    add more text.

--- src/pages/romeo-and-juliet.tsx
+++ src/pages/romeo-and-juliet.tsx
@@ -1,9 +1,9 @@
 import SEO from "$components/SEO"
-import React, { useState } from "react"
+import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import "@fontsource/eb-garamond/400.css"
 import "@fontsource/eb-garamond/700.css"
-import dayjs, { Dayjs } from "dayjs"
+import dayjs from "dayjs"
 import useCountdown from "../useCountdown"
 
 const dueDate = dayjs("2021-12-12:09:00")
@@ -14,30 +14,25 @@ const RomeoAndJuliet = () => {
   return (
     <div className={"bg-romeo-background relative xl:px-28"}>
       <SEO title={"Romeo and Juliet"} />
-
       <p className="text-xl text-romeo-text font-bold font-romeo text-center mx-auto px-8 pt-8 block md:hidden opacity-70">
         Pernikahan
       </p>
       <h1 className="text-4xl text-romeo-text font-bold font-romeo text-center mx-auto px-8 block md:hidden">
         Romeo & Juliet
       </h1>
-
       <div className="fixed right-0 top-0 flex-col items-center p-16 font-romeo hidden xl:flex">
         <p>{dueDate.month()}</p>
         <p>/</p>
         <p>{dueDate.date()}</p>
       </div>
-
       <div className="fixed left-0 top-0 flex-col items-center p-16 font-romeo hidden xl:flex">
         <p>R</p>
         <p>/</p>
         <p>J</p>
       </div>
-
       <div className="fixed text-md origin-top-left transform -rotate-90 left-0 bottom-0 flex-col items-center m-16 font-romeo hidden xl:flex">
         Denpasar, Bali
       </div>
-
       <div className={"max-w-7xl mx-auto"}>
         <div className={"md:flex"}>
           <div className={`relative p-8 md:pt-16`}>
@@ -62,17 +57,52 @@ const RomeoAndJuliet = () => {
         </div>
       </div>
 
-      <div className={"max-w-7xl mx-auto px-8 pt-8 pb-4 font-bold mt-16"}>
-        Akad Nikah
+      <div>
+        <div className={"max-w-7xl mx-auto px-8 pt-8 pb-4 font-bold"}>
+          Tanggal
+        </div>
+        <div className="max-w-7xl mx-auto px-8 font-bold text-4xl font-romeo text-romeo-text">
+          {dueDate.format("HH.mm, dddd")}
+        </div>
+        <div className="max-w-7xl mx-auto px-8 font-bold text-4xl font-romeo text-romeo-text">
+          {dueDate.format("DD MMMM ")}
+        </div>
+        <div className="max-w-7xl mx-auto px-8 font-bold text-4xl font-romeo text-romeo-text">
+          {dueDate.format("YYYY")}
+        </div>
       </div>
-      <div className="max-w-7xl mx-auto px-8 font-bold text-4xl font-romeo text-romeo-text">
-        {dueDate.format("dddd")}
-      </div>
-      <div className="max-w-7xl mx-auto px-8 font-bold text-4xl font-romeo text-romeo-text">
-        {dueDate.format("DD MMMM ")}
-      </div>
-      <div className="max-w-7xl mx-auto px-8 font-bold text-4xl font-romeo text-romeo-text">
-        {dueDate.format("YYYY")}
+
+      <div className={"flex flex-col md:flex-row max-w-7xl mx-auto p-8 mt-16"}>
+        <div className={"pb-8"}>
+          <div className={"pb-4 font-bold"}>Lokasi</div>
+          <div className="font-bold text-4xl font-romeo text-romeo-text ">
+            The Apurva Kempinski
+          </div>
+          <div className="font-bold text-4xl font-romeo text-romeo-text">
+            Denpasar, Bali
+          </div>
+        </div>
+
+        <div className={"prose max-w-sm ml-auto"}>
+          <p className={"text-xl font-romeo"}>
+              "Demikianlah mereka bukan lagi dua, melainkan satu. Karena itu,
+              apa yang telah dipersatukan Allah, tidak boleh diceraikan
+              manusia."
+          </p>
+          <p className={"font-romeo opacity-80"}>Matius 19:6 TB</p>
+          <p>
+            Tuhan membuat segala sesuatu indah pada waktunya. Indah saat Dia
+            mempertemukan, indah saat Dia menumbuhkan kasih, indah saat Dia
+            mempersatukan putra-putri kami dalam suatu ikatan pernikahan.
+          </p>
+
+          <p>
+            Dengan segala kerendahan hati dan dengan ungkapan syukur atas
+            karunia Tuhan, kami mengundang Bapak/ Ibu/ Saudara/ i untuk
+            menghadiri Resepsi Pernikahan putra-putri kami yang akan
+            diselenggarakan pada :
+          </p>
+        </div>
       </div>
     </div>
   )
